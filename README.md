fee8d246c501bd759bf8629d5625ac50f0a9658c
Leonardo Vargas <leu1607@hotmail.com>
2022-06-09 23:17:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  Interactive rating component
+</h1>
+
+<p align="center">
+  <img src=".github/preview.png" width="100%" />
+</p>
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-console.log('olar mundo')
+function selectRating(ratingId) {
+  const ratings = document.querySelectorAll('.card__rating button')
+
+  ratings.forEach((rating, index) => {
+    const isFill = index < ratingId
+    const isSelected = (index + 1) === ratingId
+
+    rating.classList.remove('active-fill')
+    rating.classList.remove('active-outline')
+
+    if(isSelected) {
+      rating.classList.add('active-fill')
+    } else if(isFill) {
+      rating.classList.add('active-outline')
+    } 
+  })
+}
